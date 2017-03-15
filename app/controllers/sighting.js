0ae7244449338ecba22436fde34ea32f355624ee
dev-jitsu <andy@purplelabs.io>
2017-03-15 09:41:25 -0400

    cleanup

--- app/controllers/sighting.js
+++ app/controllers/sighting.js
@@ -11,49 +11,5 @@ import Ember from "ember";
 export default Ember.Controller.extend({
     relationshipSorting: ['relationship_type: asc', 'name:asc'],
     sortedSourceRelationshipObjects: Ember.computed.sort('model.sourceRelationshipObjects', 'relationshipSorting'),
-    sortedTargetRelationshipObjects: Ember.computed.sort('model.targetRelationshipObjects', 'relationshipSorting'),
-    // /**
-    //  * Get Validation Errors for new record
-    //  *
-    //  * @param {Object} item Item Object with properties for validation
-    //  * @return {Array} Array of Errors
-    //  */
-
-
-    // /** @type {Object} */
-    // actions: {
-    //     /**
-    //      * Save Item after validation
-    //      *
-    //      * @function actions:save
-    //      * @param {Object} item Item Object to be created
-    //      * @return {undefined}
-    //      */
-    //     save(item, addAnother) {
-
-    //         let store = this.get("store");
-    //         let itemRecord = store.createRecord("sighting", item);
-    //         let promise = itemRecord.save();
-    //         let self = this;
-
-    //         promise.then(function() {
-    //             if (addAnother) {
-    //                 //stay on the New page
-    //                 Ember.setProperties(item, {
-    //                     summary: null,
-    //                     count: null
-    //                 });
-    //             } else {
-    //                 self.transitionToRoute("sightings");
-    //             }
-    //         });
-    //         promise.catch(function(error) {
-    //             var alert = {
-    //                 label: "Save Failed",
-    //                 error: error
-    //             };
-    //             self.set("model.alert", alert);
-    //         });
-    //     }
-    // }
+    sortedTargetRelationshipObjects: Ember.computed.sort('model.targetRelationshipObjects', 'relationshipSorting')
 });
